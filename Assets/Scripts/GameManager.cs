2c26054d9930997e45cd9d2211ddf4a569dc1cab
Trenton Gilbert <tregilbe@uat.edu>
2021-07-10 11:34:00 -0500

    Started Pickup, added gamemanager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int currentScene = 0;
+
+    public GameObject playerPrefab;
+    public GameObject player;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            Debug.LogError(message: "I tried to create a second game manager.");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevel(string levelToLoad)
+    {
+        SceneManager.LoadScene(levelToLoad);
+        // Potentially Dangerous
+        currentScene++;
+    }
+
+    public void loadLevel(int indexToLoad)
+    {
+        SceneManager.LoadScene(indexToLoad);
+        currentScene = indexToLoad;
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentScene += 1);
+    }
+}
