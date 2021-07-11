4f53ba9f5b0111a7757f2a54c2e98218aaf83e6d
GeorgeCouch <george.couch.14@gmail.com>
2021-07-11 12:51:05 -0400

    Respawn System Finished!

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -13,7 +13,7 @@ public class PlayerDeath : MonoBehaviour
             GameObject.Find("Square").GetComponent<SpriteRenderer>().enabled = false;
             gameObject.GetComponent<BoxCollider2D>().enabled = false;
             gameObject.GetComponent<PlayerMovement>().enabled = false;
-
+            gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
             StartCoroutine(Wait());
         }
     }
@@ -27,5 +27,6 @@ public class PlayerDeath : MonoBehaviour
         gameObject.GetComponent<PlayerMovement>().enabled = true; 
         gameObject.GetComponent<Rigidbody2D>().bodyType.Equals("Dynamic");
         transform.position = GameObject.Find("Respawn").transform.position;
+        gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
     }
 }
