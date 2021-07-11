6971f3c798ba1d6bd591c44473c71d66e3bc1cc5
Trenton Gilbert <tregilbe@uat.edu>
2021-07-11 12:51:00 -0500

    player movement and grav swap animation

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour
 {
+    private Animator anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Sight")
         {
             Debug.Log("Collision");
-            gameObject.GetComponent<SpriteRenderer>().enabled = false;
-            GameObject.Find("Square").GetComponent<SpriteRenderer>().enabled = false;
             gameObject.GetComponent<BoxCollider2D>().enabled = false;
             gameObject.GetComponent<PlayerMovement>().enabled = false;
             gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
@@ -20,12 +25,11 @@ public class PlayerDeath : MonoBehaviour
 
     IEnumerator Wait()
     {
+
+        anim.Play("PlayerDeath");
         yield return new WaitForSeconds(2);
-        gameObject.GetComponent<SpriteRenderer>().enabled = true;
-        GameObject.Find("Square").GetComponent<SpriteRenderer>().enabled = true;
         gameObject.GetComponent<BoxCollider2D>().enabled = true;
-        gameObject.GetComponent<PlayerMovement>().enabled = true; 
-        gameObject.GetComponent<Rigidbody2D>().bodyType.Equals("Dynamic");
+        gameObject.GetComponent<PlayerMovement>().enabled = true;
         transform.position = GameObject.Find("Respawn").transform.position;
         gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
     }
